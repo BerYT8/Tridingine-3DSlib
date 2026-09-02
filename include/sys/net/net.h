d0d31b5269fdc0bd96261d9895b157aa629b2700
BerYT8 <beryt84601@gmail.com>
2026-09-02 17:13:47 +0200

    Added net api, sockets, tcp and http/https calls

--- include/sys/net/net.h
+++ include/sys/net/net.h
@@ -1,8 +1,16 @@
 #ifndef NET_H
 #define NET_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bool net_system_init();
 
 void net_system_shutdown();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // NET_H
